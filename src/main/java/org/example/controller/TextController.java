dbee32c3a70c709aaab1f1dcce69bee934c3a787
White Rabbit Git <haaarloiii@gmail.com>
2025-09-29 16:30:15 +0300

    api

--- src/main/java/org/example/controller/TextController.java
+++ src/main/java/org/example/controller/TextController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TextController {
 
-    @GetMapping("/")
+    @GetMapping("/api")
     public String home() {
         return "API работает! Добро пожаловать.";
     }
